0d224867856a224e43084f5309e8dfbb7df7bd83
vanzhiganov <vanzhiganov@rosplatforma.ru>
2023-05-10 04:56:25 +0300

    clean code

--- front/gateway_front.py
+++ front/gateway_front.py
@@ -1,19 +1,23 @@
 import sys
-import gateway_back # .so object, written in C++ with Boost.Python
+# .so object, written in C++ with Boost.Python
+import gateway_back
 
-#simple singletone
-class Gate(object):
+
+class Gate:
     _instance = None
+
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super(Gate, cls).__new__(cls, *args, **kwargs)
         return cls._instance
+
     def get_latest(self, feed_id):
-        # take get_latest from c++ backend
+        """take get_latest from c++ backend"""
         return gateway_back.get_latest(feed_id)
 
-# to test from console
+
 def main():
+    """to test from console"""
     g = Gate()
     s = g.get_latest("test")
     print(s)
