77ba863e65c027d60e537419705219438dc0f54e
vanzhiganov <vanzhiganov@rosplatforma.ru>
2023-05-10 12:57:28 +0300

    ref

--- back/gateway_back.cpp
+++ back/gateway_back.cpp
@@ -1,9 +1,10 @@
 #include <boost/python.hpp>
-using namespace boost::python;
 
-extern char const* get_latest(char const* feedId);
+extern char const* get_latest(std::string feedId);
+// extern char const* get_latest(char const* feedId);
 
 BOOST_PYTHON_MODULE(gateway_back)
 {
+  using namespace boost::python;
   def("get_latest", get_latest);
 }
